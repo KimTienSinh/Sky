0efd34d2a279fa0772960205589b53436bb1d9a9
ADMIN <thikimhoa.py@gmail.com>
2024-01-03 16:43:21 +0700

    add Book

---
+++ SkyBook/Data/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkyBook.Data
+{
+    [Table("Book")]
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public double Price { get; set; }
+
+        [Range(0, 100)]
+        public int Quantity { get; set; }
+    }
+}
---
+++ SkyBook/Data/BookStoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkyBook.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+        #region Dbset
+        public DbSet<Book>? Books { get; set; }
+
+        #endregion
+    }
+}
